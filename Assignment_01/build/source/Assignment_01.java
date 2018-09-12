45973192db460fdac0809724e3e1499b303d37c4
CaptainHummus <Thekingofspades@live.se>
2018-09-12 11:03:36 +0200

    Added scanlines and fixing noloop

--- Assignment_01/build/source/Assignment_01.java
+++ Assignment_01/build/source/Assignment_01.java
@@ -1,19 +1,35 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Assignment_01 extends PApplet {
+
 // Assignment 1, Write my name
 int underlineX = 100;
 int scanLineDistance = 10;
 int frames = 0;
 
-void setup()
+public void setup()
 {
-  size(768, 432);
+  
 }
 
 
-void draw()
+public void draw()
 {
   background(136, 37, 42);
   stroke(3, 3, 3);
-  strokeWeight(13.9);
+  strokeWeight(13.9f);
   fill(136, 37, 42);
 
   // First Letter "H"
@@ -33,7 +49,7 @@ void draw()
   ellipse(487, 268, 70, 70);
   line(520, 268, 454, 268);
   //Sixth Letter "r"
-  arc(588, 270, 65, 65, 3.5, 5.6);
+  arc(588, 270, 65, 65, 3.5f, 5.6f);
   line(555, 300, 555, 234);
 
   //Eraser and fills
@@ -43,9 +59,9 @@ void draw()
   triangle(334, 334, 384, 334, 358, 275);
   //For the E's
   noFill();
-  arc(248, 301, 35, 35, -1.5, HALF_PI);
+  arc(248, 301, 35, 35, -1.5f, HALF_PI);
   line(252, 282, 276, 282);
-  arc(504, 301, 35, 35, -1.5, HALF_PI);
+  arc(504, 301, 35, 35, -1.5f, HALF_PI);
   line(514, 282, 530, 282);
 
   // Animated Underline
@@ -64,4 +80,14 @@ void draw()
     line(0, i + frames % scanLineDistance, width, i + frames % scanLineDistance);
   }
   frames++;
+}
+  public void settings() {  size(768, 432); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Assignment_01" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
