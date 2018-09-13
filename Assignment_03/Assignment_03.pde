5a8e5c318df4cdd2dd8e47a66c457c9112374df3
CaptainHummus <Thekingofspades@live.se>
2018-09-13 10:42:02 +0200

    Started on Assignment 3

---
+++ Assignment_03/Assignment_03.pde
@@ -0,0 +1,31 @@
+int frame = 0;
+float mutliplier = 0.04;
+int numberOfPoints = 100;
+float pi = 3.14;
+
+void setup()
+{
+size (720, 720);
+strokeWeight(8);
+}
+
+void draw()
+{
+  background(120,255,120);
+  stroke(0);
+
+  for(int i = 0; i < numberOfPoints; i++) {
+    point(100 + (i * 5), height/2 + sin((i*0.08) + frame * 0.01) * 100 );
+  }
+  stroke(255);
+
+  for(int i = 0; i < numberOfPoints; i++) {
+    point(100 + (i * 5), height/2 + cos((i*0.08) + frame * 0.01) * 100 );
+  }
+
+  // for(int i = 0; i < numberOfPoints; i++) {
+  //   point(200 + sin( (i* 0.02) + frame * 0.02)  * 100,height/2  +sin((i* 0.02) + frame * 0.04 * i) * 100);
+  //
+  // }
+  frame++;
+}
---
+++ Assignment_03/build/source/Assignment_03.java
@@ -0,0 +1,71 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Assignment_03 extends PApplet {
+
+int frame = 0;
+float mutliplier = 0.04f;
+int numberOfPoints = 100;
+float pi = 3.14f;
+
+public void setup()
+{
+
+strokeWeight(8);
+}
+
+public void draw()
+{
+  background(120,255,120);
+  stroke(0);
+
+  for(int i = 0; i < numberOfPoints; i++) {
+    point(100 + (i * 5), height/2 + sin((i*0.08f) + frame * 0.01f) * 100 );
+  }
+  stroke(255);
+
+  for(int i = 0; i < numberOfPoints; i++) {
+    point(100 + (i * 5), height/2 + cos((i*0.08f) + frame * 0.01f) * 100 );
+  }
+
+  // for(int i = 0; i < numberOfPoints; i++) {
+  //   point(200 + sin( (i* 0.02) + frame * 0.02)  * 100,height/2  +sin((i* 0.02) + frame * 0.04 * i) * 100);
+  //
+  // }
+  frame++;
+}
+
+
+public void setup()
+{
+size (720, 720);
+strokeWeight(8);
+}
+
+public void draw()
+{
+  background(120,120,255);
+  stroke(0);
+}
+  public void settings() { 
+size (720, 720); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Assignment_03" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
